3a33d3ec4203b953be7b98ee94476db2ddd54e68
sp4ceforc3 <85077306+sp4ceforc3@users.noreply.github.com>
2023-04-21 11:06:36 +0200

    -- fixed SortingLayer of camera and player/NPCs
    -- added my SpecialDanceMove

--- Assets/PlayerHandling.cs
+++ Assets/PlayerHandling.cs
@@ -11,10 +11,33 @@ public class PlayerHandling : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] SpriteRenderer sr;
     bool isDancing = false;
+    bool moving = false;
     bool scaling = false;
     bool rotating = false;
 
+    IEnumerator moveOverTime(Transform objectToMove, Vector3 newPos, float duration)
+    {
+        if (moving)
+        {
+            yield break;
+        }
+        moving = true;
+
+        float counter = 0;
+        Vector3 startPos = objectToMove.position;
+
+        while(counter < duration)
+        {
+            counter += Time.deltaTime;
+            objectToMove.position = Vector3.Lerp(startPos, newPos, counter/duration);
+            yield return null;
+        }
+
+        moving = false;
+
+    }
     //help from: https://stackoverflow.com/questions/46587150/scale-gameobject-over-time
+
     IEnumerator scaleOverTime(Transform objectToScale, Vector3 toScaleUp, Vector3 toScaleDown, float duration)
     {
         //Make sure there is only one instance of this function running
@@ -26,21 +49,21 @@ public class PlayerHandling : MonoBehaviour
 
         //Get the current scale of the object to be moved
         Vector3 startScaleSize = objectToScale.localScale;
-
-        while (danceTimer < duration)
+        float halveDuration = duration/2;
+        while (danceTimer < halveDuration)
         {
             danceTimer += Time.deltaTime;
-            objectToScale.localScale = Vector3.Lerp(startScaleSize, toScaleUp, danceTimer / duration);
+            objectToScale.localScale = Vector3.Lerp(startScaleSize, toScaleUp, danceTimer / halveDuration);
             yield return null;
         }
         danceTimer = 0f;
 
         startScaleSize = objectToScale.localScale;
 
-        while (danceTimer < duration)
+        while (danceTimer < halveDuration)
         {
             danceTimer += Time.deltaTime;
-            objectToScale.localScale = Vector3.Lerp(startScaleSize, toScaleDown, danceTimer / duration);
+            objectToScale.localScale = Vector3.Lerp(startScaleSize, toScaleDown, danceTimer / halveDuration);
             yield return null;
         }
         danceTimer = 0f;
@@ -93,6 +116,43 @@ public class PlayerHandling : MonoBehaviour
         
         isDancing = false;      
     }
+
+    IEnumerator DanceMoveLinusAdvanced()
+    {
+        if (isDancing)
+        {
+            yield break;
+        }
+        isDancing = true;
+
+        float duration = 0.5f;
+        
+        int movesCounter = 0;
+        Vector3 origPos = player.transform.position;
+        while (movesCounter < 10)
+        {
+            Vector3 startPos = player.transform.position;
+            Vector3 newPos = Random.insideUnitCircle.normalized;
+            newPos += transform.position;
+            newPos.z = player.transform.position.z;
+
+            //float angle = Mathf.Atan2(newPos.y, newPos.x) * Mathf.Rad2Deg;
+            //player.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+
+            Vector3 scaleUp = new Vector3(Random.Range(0.5f, 3f), Random.Range(0.5f, 3f), 1f);
+            StartCoroutine(moveOverTime(player.transform, newPos, duration));
+            StartCoroutine(scaleOverTime(player.transform, scaleUp, player.transform.localScale, duration));
+            player.transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+
+            yield return new WaitForSeconds(duration); 
+            movesCounter++;
+        }
+        player.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        StartCoroutine(moveOverTime(player.transform, origPos, 1f));  
+        isDancing = false;
+
+    }
     void Start()
     {
     }
@@ -106,6 +166,9 @@ public class PlayerHandling : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.Alpha1))
             StartCoroutine(DanceMoveLinusBasic());
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            StartCoroutine(DanceMoveLinusAdvanced());
+        //Dummy/Test Move at the moment
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             //StartCoroutine(scaleOverTime(player.transform, new Vector3(1.5f, 1.5f, 1f), new Vector3(1f, 1f, 1f), 0.5f));
