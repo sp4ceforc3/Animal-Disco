90ca5d105090bacd1c74c4ae16e6fd51880d6da5
Domai <tim.barsch@rub.de>
2023-04-27 10:21:02 +0200

    Domai-DanceMove-Basic
    - add a basic dance move
    - add vscode to gitignore

--- Assets/Npcs.cs
+++ Assets/Npcs.cs
@@ -8,7 +8,8 @@ public class Npcs : MonoBehaviour
     bool moving = false;
     bool scaling = false;
     bool rotating = false;
-     IEnumerator moveOverTime(Transform objectToMove, Vector3 newPos, float duration)
+    
+    IEnumerator moveOverTime(Transform objectToMove, Vector3 newPos, float duration)
     {
         if (moving)
         {
@@ -27,10 +28,9 @@ public class Npcs : MonoBehaviour
         }
 
         moving = false;
-
     }
-    //help from: https://stackoverflow.com/questions/46587150/scale-gameobject-over-time
 
+    //help from: https://stackoverflow.com/questions/46587150/scale-gameobject-over-time
     IEnumerator scaleOverTime(Transform objectToScale, Vector3 toScaleUp, Vector3 toScaleDown, float duration)
     {
         //Make sure there is only one instance of this function running
@@ -111,11 +111,57 @@ public class Npcs : MonoBehaviour
         isDancing = false;      
     }
 
+    IEnumerator DanceMoveDomaiBasic() {
+        if (isDancing)
+            yield break;
+        isDancing = true;
+
+        StartCoroutine(_danceMoveDomaiBasic(transform, new Vector3(0.5f, 2f, 1f), 0.2f));
+        yield return new WaitForSeconds(0.6f);
+
+        isDancing = false;      
+    }
+
+    private IEnumerator _danceMoveDomaiBasic(Transform objectToScale, Vector3 stretch, float split) {
+        if (scaling)
+            yield break;
+        scaling = true;
+        
+        Vector3 returnScale = objectToScale.localScale;
+        Vector3 stretchX = new Vector3(stretch.x, 0.5f, 1f);
+        Vector3 stretchY = new Vector3(1f, stretch.y, 1f);
+
+        float counter = 0f;
+        while (counter < split) {
+            counter += Time.deltaTime;
+            objectToScale.localScale = Vector3.Lerp(returnScale, stretchX, counter / split);
+            yield return null;
+        }
+
+        counter = 0f;
+        Vector3 startScale = objectToScale.localScale;
+        while (counter < split) {
+            counter += Time.deltaTime;
+            objectToScale.localScale = Vector3.Lerp(startScale, stretchY, counter / split);
+            yield return null;
+        }
+
+        counter = 0f;
+        startScale = objectToScale.localScale;
+        while (counter < split) {
+            counter += Time.deltaTime;
+            objectToScale.localScale = Vector3.Lerp(startScale, returnScale, counter / split);
+            yield return null;
+        }
+
+        scaling = false;
+    }
+
     void ChooseDance()
     {
         // TODO: Replace with Coroutine which randomly chooses a basic dance move
         // at the moment the other moves are missing for proper implementation
-        StartCoroutine(DanceMoveLinusBasic());
+        StartCoroutine(DanceMoveDomaiBasic());
 
     }
     // Start is called before the first frame update
@@ -125,8 +171,5 @@ public class Npcs : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update(){}
 }
